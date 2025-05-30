cc5b5d7b84695add50ae532c4c31ec45a20e4de9
TheRealCringengineer <cringengineer_was_taken@proton.me>
2025-05-30 11:19:09 +0300

    Move : Condition interface to separate folder

---
+++ Conditions/Condition.h
@@ -0,0 +1,44 @@
+#ifndef CONDITION_H
+#define CONDITION_H
+
+#include <cstdint>
+#include <string>
+
+#include "../Input/Input.h"
+
+enum class ConditionState : uint8_t
+{
+  Start = 0,
+  NoChange,
+  End
+};
+
+template<typename D>
+class Condition
+{
+public:
+  Condition(const std::string& n)
+      : name(n)
+  {}
+
+  virtual ConditionState CheckCondition(InputData<D>* data, float time) = 0;
+
+  virtual bool ShouldNotify() = 0;
+
+  virtual ~Condition() = default;
+
+  std::string_view GetName() { return name; }
+
+  float GetStartTime() { return startTime; }
+  float GetEndTime() { return endTime; }
+  float GetDuration() { return endTime - startTime; }
+
+protected:
+  std::string name;
+
+  float startTime = .0f;
+  float endTime = .0f;
+};
+
+
+#endif// CONDITION_H
